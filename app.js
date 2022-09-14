827a916c5b7b0b7130f38be99aa21d1a89ca2c52
pnichols789 <pnichols789@gmail.com>
2022-09-13 21:29:43 -0500

    -

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 // functionality needed:
-// ac clears prevDisplay and resets currentDisplay to 0
-// plus-minus button toggles between positive and negative for currentDisplay
-// plus-minus button does nothing if the value of currentDisplay is zero
 // percent button does nothing, I'd like it to multiply prevDisplay value by 100 and add a percent symbol
-// number button press puts number in currentDisplay
-// operator button sends currentDisplay value to prevDisplay followed by the operator that was pushed
-// decimal operator adds a decimal point to the currentDisplay
-// equal button triggers the calculation based on the values of currentDisplay, prevDisplay, and which operator was used. 
+// operator buttons send currentDisplay value to prevDisplay followed by the operator that was pushed
+// operator buttons trigger the calculation of prevDisplay, operator, and currentDisplay only if a number is already in prevDisplay. 
 // equal button assigns the result to prevDisplay and removes the operator
 
 
@@ -20,32 +15,37 @@ let sameOperator = false;
 
 controls.addEventListener('click', (e) => {
     if (!e.target.classList.contains('button')) return;
-
+    
     let currentEntry = currentDisplay.innerHTML;
     let value = e.target.innerHTML;
-
+    
+    // ac clears prevDisplay and resets currentDisplay to 0
     if (e.target.classList.contains('clear')) {
         operator = '';
         firstNum = '';
         currentDisplay.innerHTML = '0';
         prevDisplay.innerHTML = '';
     }
-
+    
+    // decimal button adds a decimal point to the currentDisplay
     if (e.target.classList.contains('decimal')) {
         if (!currentEntry.includes('.')) {
             currentDisplay.innerHTML += '.';
         }
     }
-
+    
+    // number button press puts number in currentDisplay
     if(e.target.classList.contains('number')) {
         if (currentEntry.length < 8)
-            if(currentEntry !== '0') {
-                currentDisplay.innerHTML += value;
-            } else if (value !== '0') {
-                currentDisplay.innerHTML = value;
-            }
+        if(currentEntry !== '0') {
+            currentDisplay.innerHTML += value;
+        } else if (value !== '0') {
+            currentDisplay.innerHTML = value;
+        }
     }
-
+    
+    // plus-minus button toggles between positive and negative for currentDisplay
+    // plus-minus button does nothing if the value of currentDisplay is zero
     if(e.target.classList.contains('plus-minus')) {
         if (currentEntry !== '0' && currentEntry.length < 9)
             currentDisplay.innerHTML = '-' + currentEntry;
